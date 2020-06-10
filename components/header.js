fcc0eb75a09fdd0cc4d562aa5688b11f7f979719
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-06-10 16:33:58 +0530

    Fixed navbar for smaller screen

--- components/header.js
+++ components/header.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Row, Col, Menu, Layout } from "antd";
+import { Row, Col, Menu, Layout, Popover } from "antd";
+import { UnorderedListOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import styles from "../styles/header.module.css";
 
-const AppHeader = ({ selectedKey, navbarHome, menuHome, menuItemHome }) => {
+const AppHeader = ({ selectedKey }) => {
   const navLinks = [
     { title: "Home", link: "/" },
     { title: "About", link: "/about" },
@@ -12,17 +13,34 @@ const AppHeader = ({ selectedKey, navbarHome, menuHome, menuItemHome }) => {
 
   const { Header } = Layout;
 
+  const content = (
+    <div style={{ width: "100%" }}>
+      <Menu
+        mode="vertical"
+        selectedKeys={[`${selectedKey}`]}
+        className={styles.menu}
+        justify="space-between"
+      >
+        {navLinks.map((navLink, i) => (
+          <Menu.Item key={i} className={styles.menuItem}>
+            <Link href={navLink.link}>
+              <a>{navLink.title}</a>
+            </Link>
+          </Menu.Item>
+        ))}
+      </Menu>
+    </div>
+  );
+
   return (
-    <Header
-      className={styles.navbar + (navbarHome ? ` ${styles.navbarHome}` : "")}
-    >
+    <Header className={styles.navbar}>
       <Row
         justify="space-between"
         align="middle"
         gutter={{ sm: 0, md: 24, lg: 8 }}
       >
-        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 4 }}>
-          <Row justify="center">
+        <Col xs={{ span: 22 }} sm={{ span: 22 }} md={{ span: 4 }}>
+          <Row justify="center" align="middle">
             <img src="/vercel.svg" width={128} alt="Brand Logo" />
           </Row>
         </Col>
@@ -30,24 +48,27 @@ const AppHeader = ({ selectedKey, navbarHome, menuHome, menuItemHome }) => {
           <Menu
             mode="horizontal"
             selectedKeys={[`${selectedKey}`]}
-            className={styles.menu + (menuHome ? ` ${styles.menuHome}` : "")}
+            className={styles.menu}
             justify="space-between"
           >
             {navLinks.map((navLink, i) => (
-              <Menu.Item
-                key={i}
-                md={{ span: 5 }}
-                className={
-                  styles.menuItem +
-                  (menuItemHome ? ` ${styles.menuItemHome}` : "")
-                }
-              >
+              <Menu.Item key={i} md={{ span: 5 }} className={styles.menuItem}>
                 <Link href={navLink.link}>
                   <a>{navLink.title}</a>
                 </Link>
               </Menu.Item>
             ))}
           </Menu>
+        </Col>
+        <Col xs={{ span: 2 }} sm={{ span: 2 }} md={{ span: 0 }}>
+          <Popover
+            placement="bottomRight"
+            trigger="click"
+            content={content}
+            overlayClassName={styles.navbarPopover}
+          >
+            <UnorderedListOutlined style={{ fontSize: "16px" }} />
+          </Popover>
         </Col>
       </Row>
     </Header>
