b9683b4d5ca9f7afcfef528bfdfc3a8de85d30f5
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-06-23 01:48:43 +0530

    Modified _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en" dir="ltr">
         <Head>
           <meta name="application-name" content="Joyrampur Society" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
