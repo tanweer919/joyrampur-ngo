94f8853f130d111d9af0c4bd90c06d6ee061c9fe
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-06-23 01:11:21 +0530

    Added service worker

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === "development",
     register: true,
-    scope: "/app",
-    sw: "service-worker.js"
+    scope: "/",
+    sw: "service-worker.js",
   },
 });
